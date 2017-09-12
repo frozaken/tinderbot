a0b4573cc761cfc6adcc50f98a2a52628f0cd70c
Sukini <marcumail@gmail.com>
2017-09-12 23:11:32 +0200

    better error messages

--- main.py
+++ main.py
@@ -114,8 +114,7 @@ if __name__ == "__main__":
             features.sleep(1)
 
     except KeyboardInterrupt:
-        print("Thank you, please come again :)")
-        sys.exit(1)
+        sys.exit("Thank you, please come again :)")
 
 
 
