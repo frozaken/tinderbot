c99fb4e96c54dd94c00256bc73550cd0756e41bb
Sukini <marcumail@gmail.com>
2017-11-06 13:26:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import fb_auth_token
 
-fb_username = "hubid@vipepe.com"
-fb_password = "tinderlul"
+fb_username = ""
+fb_password = ""
 fb_access_token = fb_auth_token.get_fb_access_token(fb_username, fb_password)
 fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
 
 myTinderID = ''
 myTinderName =''
 
-dbHost = "46.101.137.45"
-dbName ='TellersTinder'
-dbCollection = 'MatchedPairs'
-dbUser = 'tinderbot'
-dbPass = 'iambot'
+dbHost = ""
+dbName =''
+dbCollection = ''
+dbUser = ''
+dbPass = ''
 
 
 host = 'https://api.gotinder.com'
